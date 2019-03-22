1c1556fc3b93fe896223b26e6a9f2d444cacc2ac
Adam Georgsson <adamgeorgsson@gmail.com>
2019-03-22 13:43:58 +0100

    Add psr-2 hook

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -3,8 +3,9 @@
 
 
 echo "Running pre-commit hook"
-./bin/run-test.sh
 
+# Make sure all tests pass
+./bin/run-test.sh
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
     echo -e "\e[31mTest failed\e[0m"
@@ -12,4 +13,16 @@ if [ $? -ne 0 ]; then
     exit 1
 else
     echo -e "\e[32mTest OK!\e[0m"
+fi
+
+
+# Make sure all PSR-2 code style standard is followed
+./vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix . --dry-run
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mSomething is wrong with the code style!\e[0m"
+    echo "To see what is wrong, run vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix . --dry-run --diff"
+    echo "To automatically fix it, run vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix ."
+    exit 1
+else
+    echo -e "\e[32mCode style OK!\e[0m"
 fi
--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-$app->get('/{name}', function (Request $req,  Response $res, $args = []) {
+$app->get('/{name}', function (Request $req, Response $res, $args = []) {
     $name = $args['name'];
     return $res->getBody()->write("Hello $name");
 });
