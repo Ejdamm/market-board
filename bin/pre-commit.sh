725252b0f691cc6d4e683d0fb7dd8e0a50aa0422
Adam Georgsson <adamgeorgsson@gmail.com>
2019-03-23 10:11:55 +0100

    Merge content of run-test into pre-commit

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 
-
-
 echo "Running pre-commit hook"
 
 # Make sure all tests pass
-./bin/run-test.sh
-# $? stores exit value of the last command
+result=`composer test`
 if [ $? -ne 0 ]; then
     echo -e "\e[31mTest failed\e[0m"
+    echo $result
     echo "Tests must pass before commit!"
     exit 1
 else
     echo -e "\e[32mTest OK!\e[0m"
 fi
 
-
-# Make sure all PSR-2 code style standard is followed
+# Make sure PSR-2 code style standard is followed
 ./vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix . --dry-run
 if [ $? -ne 0 ]; then
     echo -e "\e[31mSomething is wrong with the code style!\e[0m"
-    echo "To see what is wrong, run vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix . --dry-run --diff"
-    echo "To automatically fix it, run vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix ."
+    echo "To see what is wrong, run bin/psr-2.sh -v --dry-run --diff"
+    echo "To automatically fix it, run bin/psr-2.sh"
     exit 1
 else
     echo -e "\e[32mCode style OK!\e[0m"
---
+++ bin/psr-2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+BASEDIR=$(dirname "$BASH_SOURCE")
+$BASEDIR/../vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix $1 $2 $3 $4 $BASEDIR/../
