39253a3d2f22c4fb361eadd60fdcd633e3a5105a
Adam Georgsson <adamgeorgsson@gmail.com>
2020-12-04 23:41:34 +0100

    Issue #84: Remove hardcoded links

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,6 @@ use MarketBoard\Controllers;
 $app->get('/listings/new', Controllers\NewListingController::class . ':get');
 $app->post('/listings/new', Controllers\NewListingController::class . ':post');
 $app->get('/listings[/]', Controllers\MultipleListingsController::class . ':get')->setName('listings');
-$app->get('/listings/{id}', Controllers\SingleListingController::class . ':get');
+$app->get('/listings/{id}', Controllers\SingleListingController::class . ':get')->setName('single_listing');
 $app->post('/listings/{id}', Controllers\SingleListingController::class . ':post');
 $app->get('/', Controllers\HomepageController::class . ':get')->setName('homepage');
