b51d0bb5035b4818962e3a8b9c4176d86c2b1b03
Adam Georgsson <adamgeorgsson@gmail.com>
2021-03-25 21:43:25 +0100

    Put domain in config

--- src/EmailNewListing.php
+++ src/EmailNewListing.php
@@ -8,18 +8,20 @@ use Anddye\Mailer\Mailable;
 class EmailNewListing extends Mailable
 {
     private $emailFields;
+    private $domain;
 
-    public function __construct($emailFields, $subject)
+    public function __construct($emailFields, $subject, $domain)
     {
         $this->emailFields = $emailFields;
         $this->setSubject($subject);
+        $this->domain = $domain;
     }
 
     public function build()
     {
         $this->setView('emails/new_listing_confirmation.html.twig', [
             'params' => $this->emailFields,
-            'server_address' => $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'],
+            'server_address' => $_SERVER['REQUEST_SCHEME'] . "://" . $this->domain,
         ]);
 
         return $this;
--- src/EmailSeller.php
+++ src/EmailSeller.php
@@ -8,18 +8,20 @@ use Anddye\Mailer\Mailable;
 class EmailSeller extends Mailable
 {
     private $emailFields;
+    private $domain;
 
-    public function __construct($emailFields, $subject)
+    public function __construct($emailFields, $subject, $domain)
     {
         $this->emailFields = $emailFields;
         $this->setSubject($subject);
+        $this->domain = $domain;
     }
 
     public function build()
     {
         $this->setView('emails/email_seller.html.twig', [
             'params' => $this->emailFields,
-            'server_address' => $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'],
+            'server_address' => $_SERVER['REQUEST_SCHEME'] . "://" . $this->domain,
         ]);
 
         return $this;
